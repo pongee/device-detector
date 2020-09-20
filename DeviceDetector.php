970732176dcb7e8072a4892bf2a9b9e8429b79eb
pongee <36171363+pongee@users.noreply.github.com>
2020-09-20 20:43:23 +0200

    feat(tag) Tag update to 3.16.4

--- DeviceDetector.php
+++ DeviceDetector.php
@@ -50,7 +50,7 @@ class DeviceDetector
     /**
      * Current version number of DeviceDetector
      */
-    const VERSION = '3.16.3';
+    const VERSION = '3.16.4';
 
     /**
      * Holds all registered client types
